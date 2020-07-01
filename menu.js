5cbbb1937c09a6738fe54e6482d77f8d2728f6b5
Cesar Machado <cesarmachado8707@gmail.com>
2020-07-01 18:56:47 -0400

    Add files via upload
    # Conflicts:
    #	style.css

--- menu.js
+++ menu.js
@@ -1,27 +1,19 @@
 window.addEventListener('load', () => {
     let menuDropDown = document.getElementById('user')
     let subNavBar = document.getElementById('subnavbar')
-    let colorDropDown = document.getElementById('colorTheme')
-    let colorNavBar = document.getElementById('colorSubNav')
-    // const darkMode = document.getElementById('checkbox')
+    let darkMode = document.getElementById('checkbox')
+    
 
 
     menuDropDown.addEventListener('click', () => {
        subNavBar.classList.toggle('visible') 
     })
 
-    colorDropDown.addEventListener('click', () => {
-       colorNavBar.classList.toggle('visible') 
-    })
+    // const checkbox = document.getElementById('checkbox')
 
     darkMode.addEventListener('click', () => {
-        document.body.classList.toggle('dark');
+        document.body.classList.toggle('dark')
     })
 
 })
 
-const checkbox = document.getElementById('checkbox')
-
-checkbox.addEventListener('click', () => {
-document.body.classList.toggle('dark');
-})
