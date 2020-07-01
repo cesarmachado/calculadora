e1d664efa96930329e65e3cc779b4d0a8c7be97a
Cesar Machado <cesarmachado8707@gmail.com>
2020-07-01 16:45:43 -0400

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,8 +1,27 @@
 window.addEventListener('load', () => {
     let menuDropDown = document.getElementById('user')
     let subNavBar = document.getElementById('subnavbar')
+    let colorDropDown = document.getElementById('colorTheme')
+    let colorNavBar = document.getElementById('colorSubNav')
+    // const darkMode = document.getElementById('checkbox')
+
 
     menuDropDown.addEventListener('click', () => {
-       subNavBar.classList.toggle('visible')
+       subNavBar.classList.toggle('visible') 
     })
+
+    colorDropDown.addEventListener('click', () => {
+       colorNavBar.classList.toggle('visible') 
+    })
+
+    darkMode.addEventListener('click', () => {
+        document.body.classList.toggle('dark');
+    })
+
+})
+
+const checkbox = document.getElementById('checkbox')
+
+checkbox.addEventListener('click', () => {
+document.body.classList.toggle('dark');
 })
