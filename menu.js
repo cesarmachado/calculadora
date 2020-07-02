2d869099d5ebad4989b06ee2e45219d7dcdadfff
Cesar A <cesarmachado8707@gmail.com>
2020-07-01 20:27:44 -0400

    dark-theme

--- menu.js
+++ menu.js
@@ -1,8 +1,19 @@
 window.addEventListener('load', () => {
     let menuDropDown = document.getElementById('user')
     let subNavBar = document.getElementById('subnavbar')
+    let darkMode = document.getElementById('checkbox')
+    
+
 
     menuDropDown.addEventListener('click', () => {
-       subNavBar.classList.toggle('visible')
+       subNavBar.classList.toggle('visible') 
     })
+
+    // const checkbox = document.getElementById('checkbox')
+
+    darkMode.addEventListener('click', () => {
+        document.body.classList.toggle('dark')
+    })
+
 })
+
